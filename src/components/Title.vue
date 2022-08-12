44e9bf1a7716cfa7ec3734f49f925d1cb4490f92
dargy <76412158+dragonismcode@users.noreply.github.com>
2022-08-12 23:53:42 +0000

    adding title and general things

---
+++ src/components/Title.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hero-simple-centered">
+    <div
+      class="mt-10 mb-12 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32"
+    >
+      <div class="text-center">
+        <h2
+          class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl"
+        >
+          Hi, I'm
+
+          <br class="xl:hidden" />
+          <span class="text-primary-500"> Dragonism </span>
+        </h2>
+        <p
+          class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+        ></p>
+        <div class="text-long">
+          <p class="text-gray-500">
+            I'm a developer that likes making websites, games, and much more.
+          </p>
+        </div>
+
+        <p></p>
+        <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+          <div class="rounded-md shadow">
+            <div>
+              <a
+                href="/en/benefits"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-500 hover:bg-red-400 focus:outline-none focus:ring-primary transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+                >Learn More</a
+              >
+            </div>
+          </div>
+          <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+            <div>
+              <a
+                href="/en/store"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-primary-500 bg-white hover:text-red-400 focus:outline-none focus:ring-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+                >Projects</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
